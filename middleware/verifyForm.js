5ba9610c31f85abbd078df8d05f6e0d8014f2c2b
NopXx <khamkokaewn1@hotmail.com>
2022-12-13 18:19:02 +0700

    update: add verify user address del

--- middleware/verifyForm.js
+++ middleware/verifyForm.js
@@ -47,7 +47,17 @@ module.exports = {
       !req.body.tel
     ) {
       return res.status(400).send({
-        message: 'Enter title or address or sub_district or district or province or tel'
+        message:
+          'Enter title or address or sub_district or district or province or tel'
+      })
+    } else {
+      next()
+    }
+  },
+  Useradd_del: (req, res, next) => {
+    if (!req.params.user_address_id) {
+      return res.status(400).send({
+        message: 'Enter Params user_address_id'
       })
     } else {
       next()
