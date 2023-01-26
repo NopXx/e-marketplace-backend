988b95750a3ba61bf0f3770c443e1a1aaf2e9867
NopXx <khamkokaewn1@hotmail.com>
2023-01-26 23:22:32 +0700

    update ♻️: token expire 1h to 2h

--- routes/Auth.js
+++ routes/Auth.js
@@ -77,7 +77,7 @@ router.post('/login', userMiddleware.validateRegister, (req, res, next) => {
           }
           const secret = process.env.ACCESS_TOKEN_SECRET
           const options = {
-            expiresIn: '1h'
+            expiresIn: '2h'
           }
           jwt.sign(userData, secret, options, (err, token) => {
             if (err) {
