e7dafc10140ec6476b0536b350e8742ca6b01b1c
NopXx <khamkokaewn1@hotmail.com>
2023-01-26 23:24:42 +0700

    update ♻️✅: add get product detail

--- routes/Product.js
+++ routes/Product.js
@@ -27,7 +27,26 @@ router.get('/product/search', (req, res) => {
 // get Product All
 router.get('/product', (req, res) => {
   db.query(
-    `select product.*, db_image.image from product LEFT JOIN db_image ON db_image.product_id = product.product_id;`,
+    `select product.*, db_image.image from product LEFT JOIN db_image ON db_image.product_id = product.product_id where product_show = 1 AND db_image.default_image = 1;`,
+    (err, data) => {
+      if (err) {
+        return res.status(401).send({
+          message: err.message
+        })
+      } else {
+        return res.status(200).json({
+          data: data,
+          total: data.length
+        })
+      }
+    }
+  )
+})
+// get Product By ID
+router.get('/product/:id', (req, res) => {
+  const product_id = req.params.id
+  db.query(
+    `select * from product where product_id = ${product_id};`,
     (err, data) => {
       if (err) {
         return res.status(401).send({
@@ -44,10 +63,10 @@ router.get('/product', (req, res) => {
 })
 
 // get Product By Store ID
-router.get('/product/:id', (req, res) => {
+router.get('/product/store/:id', (req, res) => {
   const id = req.params.id
   db.query(
-    `select product.*, db_image.image from product LEFT JOIN db_image ON db_image.product_id = product.product_id where product.store_id = ${id};`,
+    `select product.*, db_image.image from product LEFT JOIN db_image ON db_image.product_id = product.product_id where product.store_id = ${id} AND db_image.default_image = 1 ;`,
     (err, data) => {
       if (err) {
         return res.status(401).send({
@@ -62,9 +81,23 @@ router.get('/product/:id', (req, res) => {
     }
   )
 })
+
+// get Product Detail
+router.get('/product/detail/:id', (req, res) => {
+  db.query(`select * from product_detail where product_id = ${req.params.id}`, (err, data) => {
+    if (err) {
+      return res.status(401).json({
+        message: err.message
+      })
+    } else {
+      return res.status(200).json(data)
+    }
+  })
+})
 // create Product
 router.post('/product', [authJwt.verifyToken, authJwt.isStore], (req, res) => {
   const user_id = req.user.user_id
+  const py_id = req.body.product_type_id
   const product_name = req.body.product_name
   const product_price = req.body.product_price
   const product_number = req.body.product_number
@@ -79,7 +112,7 @@ router.post('/product', [authJwt.verifyToken, authJwt.isStore], (req, res) => {
       } else {
         store_id = data[0].store_id
         db.query(
-          `insert into product (product_name, product_price, store_id, product_number) values ('${product_name}', '${product_price}', ${store_id}, '${product_number}');`,
+          `insert into product (py_id, product_name, product_price, store_id, product_number, post_date) values (${py_id},'${product_name}', '${product_price}', ${store_id}, '${product_number}', now());`,
           (err, result) => {
             if (err) {
               return res.status(401).send({
@@ -105,7 +138,6 @@ router.post(
   [authJwt.verifyToken, authJwt.isStore],
   (req, res) => {
     const product_id = req.params.product_id
-    const py_id = req.body.product_type_id
     const pd_description = req.body.product_detail
     db.query(
       `select * from product where product_id = ${product_id}`,
@@ -121,7 +153,7 @@ router.post(
             })
           } else {
             db.query(
-              `insert into product_detail (product_id, py_id, pd_description) values (${product_id}, ${py_id}, '${pd_description}');`,
+              `insert into product_detail (product_id, pd_description) values (${product_id}, '${pd_description}');`,
               (err, data) => {
                 if (err) {
                   return res.status(401).send({
@@ -147,6 +179,7 @@ router.patch(
   [authJwt.verifyToken, authJwt.isStore],
   (req, res) => {
     const product_id = req.params.product_id
+    const py_id = req.body.product_type_id
     const product_name = req.body.product_name
     const product_price = req.body.product_price
     const product_number = req.body.product_number
@@ -164,7 +197,7 @@ router.patch(
             })
           } else {
             db.query(
-              `update product set product_name = '${product_name}', product_price = '${product_price}', product_number = ${product_number} where product_id = ${product_id}`,
+              `update product set py_id = ${py_id}, product_name = '${product_name}', product_price = '${product_price}', product_number = ${product_number} where product_id = ${product_id}`,
               (err, result) => {
                 if (err) {
                   return res.status(401).send({
@@ -190,7 +223,7 @@ router.patch(
   [authJwt.verifyToken, authJwt.isStore],
   (req, res) => {
     const product_id = req.params.product_id
-    const py_id = req.body.product_type_id
+
     const pd_description = req.body.product_detail
     db.query(
       `select * from product where product_id = ${product_id};`,
@@ -206,7 +239,7 @@ router.patch(
             })
           } else {
             db.query(
-              `update product_detail set py_id = ${py_id}, pd_description = '${pd_description}' where product_id = ${product_id};`,
+              `update product_detail set pd_description = '${pd_description}' where product_id = ${product_id};`,
               (err, result) => {
                 if (err) {
                   return res.status(401).send({
@@ -215,6 +248,86 @@ router.patch(
                 } else {
                   return res.status(200).send({
                     message: 'Updated successfully'
+                  })
+                }
+              }
+            )
+          }
+        }
+      }
+    )
+  }
+)
+
+// Product Show
+router.delete(
+  '/product/show/:id',
+  [authJwt.verifyToken, authJwt.isStore],
+  (req, res) => {
+    const product_id = req.params.id
+    db.query(
+      `select * from product where product_id = ${product_id};`,
+      (err, data) => {
+        if (err) {
+          return res.status(401).send({
+            message: err.message
+          })
+        } else {
+          if (data.length === 0) {
+            return res.status(404).send({
+              message: 'Product ID not found'
+            })
+          } else {
+            db.query(
+              `update product set product_show = 0 where product_id = ${product_id}`,
+              (err, result) => {
+                if (err) {
+                  return res.status(401).json({
+                    message: err.message
+                  })
+                } else {
+                  return res.status(200).json({
+                    message: "Updated product successfully"
+                  })
+                }
+              }
+            )
+          }
+        }
+      }
+    )
+  }
+)
+
+// Product Show
+router.patch(
+  '/product/show/:id',
+  [authJwt.verifyToken, authJwt.isStore],
+  (req, res) => {
+    const product_id = req.params.id
+    db.query(
+      `select * from product where product_id = ${product_id};`,
+      (err, data) => {
+        if (err) {
+          return res.status(401).send({
+            message: err.message
+          })
+        } else {
+          if (data.length === 0) {
+            return res.status(404).send({
+              message: 'Product ID not found'
+            })
+          } else {
+            db.query(
+              `update product set product_show = 1 where product_id = ${product_id}`,
+              (err, result) => {
+                if (err) {
+                  return res.status(401).json({
+                    message: err.message
+                  })
+                } else {
+                  return res.status(200).json({
+                    message: "Updated product successfully"
                   })
                 }
               }
