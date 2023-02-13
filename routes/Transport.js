5637631ba6c8d58628a443d56bf44cec0c363033
NopXx <khamkokaewn1@hotmail.com>
2023-02-14 02:51:15 +0700

    update♻️✅: 💠add get /transport/:id 💠add patch /transport/:id

--- routes/Transport.js
+++ routes/Transport.js
@@ -4,27 +4,78 @@ const authJwt = require('../middleware/authJwt')
 const db = require('../lib/db.js')
 
 router.get('/transport', (req, res) => {
-    db.query(`select * from transport`, (err, data) => {
-        if (err) {
-            return res.status(401).send(err);
-        } else {
-            return res.status(200).send(data);
-        }
-    })
+  db.query(
+    `SELECT transport.*, db_image.image FROM transport LEFT JOIN db_image ON db_image.transport_id = transport.transport_id`,
+    (err, data) => {
+      if (err) {
+        return res.status(401).send(err)
+      } else {
+        return res.status(200).send(data)
+      }
+    }
+  )
 })
-
-router.post('/transport', [authJwt.verifyToken, authJwt.isAdmin], (req, res) => {
-    const transport_name = req.body.transport_name
-
-    db.query(`insert into transport (transport_name) values ('${transport_name}')`, (err, result) => {
+// get by id
+router.get(
+  '/transport/:id',
+  [authJwt.verifyToken, authJwt.isAdmin],
+  (req, res) => {
+    db.query(
+      `SELECT transport.*, db_image.image FROM transport 
+      LEFT JOIN db_image ON db_image.transport_id = transport.transport_id 
+      where transport.transport_id = ${req.params.id}`,
+      (err, data) => {
         if (err) {
-            return res.status(401).send(err);
+          return res.status(401).send(err)
         } else {
-            return res.status(201).send({
-                message: 'insert successfully'
-            })
+          return res.status(200).send(data)
         }
-    })
-})
+      }
+    )
+  }
+)
+
+// create transport
+router.post(
+  '/transport',
+  [authJwt.verifyToken, authJwt.isAdmin],
+  (req, res) => {
+    const transport_name = req.body.name
+
+    db.query(
+      `insert into transport (transport_name) values ('${transport_name}')`,
+      (err, result) => {
+        if (err) {
+          return res.status(401).send(err)
+        } else {
+          return res.status(201).send({
+            message: 'insert successfully'
+          })
+        }
+      }
+    )
+  }
+)
+
+router.patch(
+  '/transport/:id',
+  [authJwt.verifyToken, authJwt.isAdmin],
+  (req, res) => {
+    const transport_name = req.body.name
+
+    db.query(
+      `update transport set transport_name = '${transport_name}' where transport_id =${req.params.id}`,
+      (err, result) => {
+        if (err) {
+          return res.status(401).send(err)
+        } else {
+          return res.status(200).send({
+            message: 'update successfully'
+          })
+        }
+      }
+    )
+  }
+)
 
 module.exports = router
