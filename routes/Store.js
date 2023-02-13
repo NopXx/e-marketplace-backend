4b5f52bf8a3d83ef856f344cf0a988c1e81067b9
NopXx <khamkokaewn1@hotmail.com>
2023-02-13 10:42:34 +0700

    update♻️✅: new query and new return data

--- routes/Store.js
+++ routes/Store.js
@@ -7,18 +7,12 @@ const db = require('../lib/db.js')
 router.get('/store/search/username', [authJwt.verifyToken], (req, res) => {
   const store_username = req.query.store_username
   db.query(
-    `select * from store where store_username like '%${store_username}%';`,
-    (err, result) => {
+    `select * from store where store_username = '${store_username}';`,
+    (err, data) => {
       if (err) {
-        return res.status(401).send({
-          message: err.message,
-          code: err.code
-        })
+        return res.status(401).send(err)
       } else {
-        return res.status(200).send({
-          result,
-          total: result.length
-        })
+        return res.status(200).send(data)
       }
     }
   )
@@ -49,22 +43,24 @@ router.get('/store/search', (req, res) => {
 // getStore by id
 router.get('/store/:store_id', (req, res) => {
   const store_id = req.params.store_id
-  db.query(`select * from store where store_id = ${store_id}`, (err, data) => {
-    if (err) {
-      return res.status(401).send({
-        message: err.message
-      })
-    } else {
-      if (data.length === 0) {
-        return res.status(404).send({
-          message: 'Store not found'
-        })
+  db.query(
+    `select store.*, store_detail.*, db_image.image from store 
+  LEFT JOIN store_detail ON store_detail.store_id = store.store_id 
+  LEFT JOIN db_image ON db_image.store_id = store.store_id
+  where store.store_id = ${store_id}`,
+    (err, data) => {
+      if (err) {
+        return res.status(401).send(err)
+      } else {
+        if (data.length === 0) {
+          return res.status(404).send({
+            message: 'Store not found'
+          })
+        }
+        return res.status(200).send(data)
       }
-      return res.status(200).send({
-        data
-      })
     }
-  })
+  )
 })
 
 // getStore All
@@ -104,22 +100,27 @@ router.post('/store', [authJwt.verifyToken], (req, res) => {
             `insert into store (store_username, store_name) values ('${store_username}', '${store_name}');`,
             (err, result) => {
               if (err) {
-                return res.status(401).send({
-                  message: err.message
-                })
+                return res.status(401).send(err)
               } else {
                 const store_id = result.insertId
                 db.query(
                   `insert into user_role (role_id, user_id, store_id) values ( 3, ${user_id}, ${store_id});`,
                   (err, result1) => {
                     if (err) {
-                      return res.status(401).send({
-                        message: err.message
-                      })
+                      return res.status(401).send(err)
                     } else {
-                      return res.status(201).send({
-                        message: 'Sign up successfully'
-                      })
+                      db.query(
+                        `insert into store_detail (store_id) values (${store_id})`,
+                        (err, result2) => {
+                          if (err) {
+                            return res.status(401).send(err)
+                          } else {
+                            return res.status(201).send({
+                              message: 'Sign up successfully'
+                            })
+                          }
+                        }
+                      )
                     }
                   }
                 )
@@ -198,6 +199,12 @@ router.patch('/store/name/:store_id', [authJwt.verifyToken], (req, res) => {
   const store_id = req.params.store_id
   const store_username = req.body.store_username
   const store_name = req.body.store_name
+  const address = req.body.address
+  const sub_district = req.body.sub_district
+  const district = req.body.district
+  const province = req.body.province
+  const tel = req.body.tel
+  const email = req.body.email
 
   db.query(`select * from store where store_id = ${store_id};`, (err, data) => {
     if (err) {
@@ -219,9 +226,18 @@ router.patch('/store/name/:store_id', [authJwt.verifyToken], (req, res) => {
                   message: err.message
                 })
               } else {
-                return res.status(200).send({
-                  message: 'Update successfully'
-                })
+                db.query(
+                  `update store_detail set address = '${address}', sub_district = '${sub_district}', district = '${district}', province = '${province}', tel = '${tel}', email = '${email}' where store_id = ${store_id};`,
+                  (err, result) => {
+                    if (err) {
+                      return res.status(401).send(err)
+                    } else {
+                      return res.status(200).send({
+                        message: 'Updated successfully'
+                      })
+                    }
+                  }
+                )
               }
             }
           )
@@ -247,9 +263,18 @@ router.patch('/store/name/:store_id', [authJwt.verifyToken], (req, res) => {
                           message: err.message
                         })
                       } else {
-                        return res.status(200).send({
-                          message: 'Update successfully'
-                        })
+                        db.query(
+                          `update store_detail set address = '${address}', sub_district = '${sub_district}', district = '${district}', province = '${province}', tel = '${tel}', email = '${email}' where store_id = ${store_id};`,
+                          (err, result) => {
+                            if (err) {
+                              return res.status(401).send(err)
+                            } else {
+                              return res.status(200).send({
+                                message: 'Updated successfully'
+                              })
+                            }
+                          }
+                        )
                       }
                     }
                   )
@@ -264,45 +289,6 @@ router.patch('/store/name/:store_id', [authJwt.verifyToken], (req, res) => {
 })
 
 // Update Store Detail
-router.patch('/store/detail/:store_id', [authJwt.verifyToken], (req, res) => {
-  const user_id = req.user.user_id
-  const store_id = req.params.store_id
-  const address = req.body.address
-  const sub_district = req.body.sub_district
-  const district = req.body.district
-  const province = req.body.province
-  const tel = req.body.tel
-  const email = req.body.email
-
-  db.query(`select * from store where store_id = ${store_id};`, (err, data) => {
-    if (err) {
-      return res.status(401).send({
-        message: err.message
-      })
-    } else {
-      if (data.length === 0) {
-        return res.status(404).send({
-          message: 'Store not found'
-        })
-      } else {
-        db.query(
-          `update store_detail set address = '${address}', sub_district = '${sub_district}', district = '${district}', province = '${province}', tel = '${tel}', email = '${email}' where store_id = ${store_id};`,
-          (err, result) => {
-            if (err) {
-              return res.status(401).send({
-                message: err.message
-              })
-            } else {
-              return res.status(200).send({
-                message: 'Updated successfully'
-              })
-            }
-          }
-        )
-      }
-    }
-  })
-})
 
 // User Request Delete Store
 router.patch('/store/del/:store_id', [authJwt.verifyToken], (req, res) => {
