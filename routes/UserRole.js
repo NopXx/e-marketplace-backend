d303c5a51d0c80acd0a7561d747f2a1999eda2d0
NopXx <khamkokaewn1@hotmail.com>
2023-02-16 01:23:26 +0700

    update: add post & delete

--- routes/UserRole.js
+++ routes/UserRole.js
@@ -46,6 +46,42 @@ router.get(
   }
 )
 
+// add role admin
+router.post(
+  '/userrole/admin',
+  [authJwt.verifyToken, authJwt.isAdmin],
+  (req, res) => {
+    const user_id = req.body.user_id
+    db.query(
+      `select * from user_role where user_id = ${user_id} and role_id = 2`,
+      (err, data) => {
+        if (err) {
+          return res.status(401).send(err)
+        } else {
+          if (data.length > 0) {
+            return res.status(403).send({
+              message: 'The user already has the administrator role.'
+            })
+          } else {
+            db.query(
+              `insert into user_role (user_id, role_id) values (${user_id}, 2)`,
+              (err, result) => {
+                if (err) {
+                  return res.status(401).send(err)
+                } else {
+                  return res.status(201).send({
+                    message: 'created user role successfully'
+                  })
+                }
+              }
+            )
+          }
+        }
+      }
+    )
+  }
+)
+
 router.patch(
   '/userrole/:user_id',
   [authJwt.verifyToken, authJwt.isAdmin],
@@ -75,6 +111,42 @@ router.patch(
                   )
                   return res.status(200).send({
                     message: 'update succeeded'
+                  })
+                }
+              }
+            )
+          }
+        }
+      }
+    )
+  }
+)
+
+// drop role user
+router.delete(
+  '/userrole/admin/:user_id',
+  [authJwt.verifyToken, authJwt.isAdmin],
+  (req, res) => {
+    const user_id = req.params.user_id
+    db.query(
+      `select * from user_role where user_id = ${user_id} and role_id = 2`,
+      (err, data) => {
+        if (err) {
+          return res.status(401).send(err)
+        } else {
+          if (data.length === 0) {
+            return res.status(404).send({
+              message: 'user not found'
+            })
+          } else {
+            db.query(
+              `delete from user_role where user_id = ${user_id} and role_id = 2`,
+              (err, result) => {
+                if (err) {
+                  return res.status(401).send(err)
+                } else {
+                  return res.status(200).send({
+                    message: 'deleted role successfully'
                   })
                 }
               }
